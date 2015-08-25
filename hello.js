159f76b16faa492aa2291bb098c69b2af5185af4
Komal Rathore <komal.rathore40@gmail.com>
2015-08-25 10:05:30 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,20 @@
 
+<!DOCTYPE html>
+<html>
+<script src= "http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+<body>
+
+<!-- IT must bedeclare here -->
+<div ng-app="">
+ 
+<p>Input something in the input box:</p>
+
+
+<p>Name: <input type="text" ng-model="name"></p>
+
+<!-- Here Binding is done -->
+<div ng-bind="name"></div>
+</div>
+
+</body>
+</html>
